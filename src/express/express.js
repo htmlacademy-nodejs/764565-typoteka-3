7d056b6f1e4c106fc59cede4f2f9ddfc722acbcb
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2021-10-07 09:29:20 +0300

    Убраны лишние комменты

--- src/express/express.js
+++ src/express/express.js
@@ -6,15 +6,12 @@ const offersRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
-// Зафиксируем порт для сервера
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
-// Подключим созданные маршруты
 app.use(`/articles`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
-// Запуск сервера
 app.listen(DEFAULT_PORT);
