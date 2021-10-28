f12a8e2a22c9aab386d0de7a7b02409e8de790bc
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2021-10-28 17:30:13 +0300

    Реализация singleton модели

--- src/service/api/sequelize.js
+++ src/service/api/sequelize.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Sequelize = require(`sequelize`);
+/*
 const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
 
 const somethingIsNotDefined = [DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT].some((it) => it === undefined);
@@ -21,4 +22,37 @@ module.exports = new Sequelize(
         idle: 10000
       }
     }
-);
+);*/
+
+let DBModule = (function () {
+  let instance;
+
+  let createInstance = function () {
+    const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
+    const somethingIsNotDefined = [DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT].some((it) => it === undefined);
+    if (somethingIsNotDefined) {
+      throw new Error(`One or more environmental variables are not defined`);
+    }
+    return new Sequelize(
+        DB_NAME, DB_USER, DB_PASSWORD, {
+          host: DB_HOST,
+          port: DB_PORT,
+          dialect: `postgres`,
+          pool: {
+            max: 5,
+            min: 0,
+            acquire: 10000,
+            idle: 10000
+          }
+        }
+    );
+  };
+
+  return {
+    getInstance() {
+      return instance || (instance = createInstance());
+    }
+  };
+});
+
+module.exports = DBModule;
