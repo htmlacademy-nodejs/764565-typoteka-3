e061d88eb8884d856cf386cd7899303b64f6c41b
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2021-11-11 19:25:25 +0300

    Настроена авторизация + сессии + csrf

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -2,6 +2,7 @@
 
 const {Router} = require(`express`);
 const upload = require(`../middlewares/upload`);
+const auth = require(`../middlewares/auth`);
 const {prepareErrors} = require(`../../utils`);
 
 const mainRouter = new Router();
@@ -11,6 +12,7 @@ const api = require(`../api`).getAPI();
 const ARTICLES_PER_PAGE = 8;
 
 mainRouter.get(`/`, async (req, res) => {
+  const {user} = req.session;
   let {page = 1} = req.query;
   page = +page;
 
@@ -27,12 +29,16 @@ mainRouter.get(`/`, async (req, res) => {
   ]);
 
   const totalPages = Math.ceil(count / ARTICLES_PER_PAGE);
-  res.render(`main`, {articles, page, totalPages, categories});
+  res.render(`main`, {articles, page, totalPages, categories, user});
 });
 
-mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
+mainRouter.get(`/register`, (req, res) => {
+  const {user} = req.session;
+  res.render(`sign-up`, {user});
+});
 
 mainRouter.post(`/register`, upload.single(`avatar`), async (req, res) => {
+  const {user} = req.session;
   const {body, file} = req;
   const userData = {
     avatar: file ? file.filename : ``,
@@ -48,27 +54,52 @@ mainRouter.post(`/register`, upload.single(`avatar`), async (req, res) => {
     res.redirect(`/login`);
   } catch (errors) {
     const validationMessages = prepareErrors(errors);
-    res.render(`sign-up`, {validationMessages});
+    res.render(`sign-up`, {user, validationMessages});
   }
 });
 
-mainRouter.get(`/login`, (req, res) => res.render(`login`));
+mainRouter.get(`/login`, (req, res) => {
+  const {user} = req.session;
+  res.render(`login`, {user});
+});
+
+mainRouter.post(`/login`, async (req, res) => {
+  try {
+    const user = await api.auth(req.body.email, req.body.password);
+    req.session.user = user;
+    req.session.save(() => {
+      res.redirect(`/`);
+    });
+  } catch (errors) {
+    const validationMessages = prepareErrors(errors);
+    const {user} = req.session;
+    res.render(`login`, {user, validationMessages});
+  }
+});
+
+mainRouter.get(`/logout`, (req, res) => {
+  delete req.session.user;
+  res.redirect(`/`);
+});
 
 mainRouter.get(`/search`, async (req, res) => {
+  const {user} = req.session;
   try {
     const {query} = req.query;
     const results = await api.search(query);
 
     res.render(`search`, {
-      results
+      results,
+      user
     });
   } catch (error) {
     res.render(`search`, {
-      results: []
+      results: [],
+      user
     });
   }
 });
 
-mainRouter.get(`/categories`, (req, res) => res.render(`all-categories`));
+mainRouter.get(`/categories`, auth, (req, res) => res.render(`all-categories`));
 
 module.exports = mainRouter;
